e28661905cd434d81eb2290a5f226d0434815307
Mateusz Połeć <polec0@wp.pl>
2019-10-25 09:43:27 +0200

    Kartkowka 2

---
+++ kartkowka2/zadanie1.py
@@ -0,0 +1,4 @@
+tuple = ('P', 'y', 't', 'h', 'o', 'n')
+ans = ''.join(tuple)
+
+print(ans)
---
+++ kartkowka2/zadanie2.py
@@ -0,0 +1,7 @@
+lista = list([1,1,2,4,6,5,3,1])
+
+max_value = lista.index(max(lista))
+min_value = lista.index(min(lista))
+
+print("Maksymalna wartosc (index): ", max_value)
+print("Minimalna wartosc (index): ", min_value)
