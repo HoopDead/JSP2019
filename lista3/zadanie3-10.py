aba2d63fe128330e0f9a5d37d63d48b715958830
HoopDead <polec0@wp.pl>
2019-10-30 00:11:22 +0100

    Lista 3

--- lista3/zadanie3-10.py
+++ lista3/zadanie3-10.py
@@ -1,8 +1,16 @@
 arr = []
+
 i = 100
-while(i <= 400):
-    if(i%2 == 0):
-        arr.append(i)
-    i = i+1
+
+while (i <= 400):
+    string = str(i)
+    check = 0
+    for j in range (3):
+        if(int(string[j]) % 2 == 0):
+            check = check + 1
+    if(check == len(string)):
+        arr.append(string)
+    i =  i+1
+    check = 0
 
 print(arr)
