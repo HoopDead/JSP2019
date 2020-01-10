e330154f82d61080ed11f37536a358d1852923c7
HoopDead <polec0@wp.pl>
2020-01-10 10:00:33 +0100

    Lista 10

---
+++ lista10/zadanie4.py
@@ -0,0 +1,51 @@
+import xml.etree.ElementTree as ET
+
+class Kursy:
+    def __init__(self,source):
+        mytree = ET.parse(source)
+        myroot = mytree.getroot()
+        self.Name = ['Polski Złoty']
+        self.Converter = [1]
+        self.Code = ['PLN']
+        self.Course = ['1,0000']
+        for x in myroot:
+            if x.tag == 'pozycja':
+                self.Name.append(x[0].text)
+                self.Converter.append(x[1].text)
+                self.Code.append(x[2].text)
+                self.Course.append(x[3].text)
+    def ListaKursow(self):
+        print("Kursy: \n")
+        print("Waluta", "Przelicznik", "Kod", "Kurs średni", sep="\t\t\t",end='\n\n')
+        for x in range(0,len(self.Name)):
+            print(self.Name[x],end="")
+            for y in range(0,(35-len(self.Name[x]))):
+                print(' ',end='')
+            if(int(self.Converter[x])>100):
+                print(self.Converter[x], '{}\t'.format(self.Code[x]), self.Course[x], sep="\t\t")
+            else:
+                print(self.Converter[x],self.Code[x],self.Course[x],sep="\t\t\t")
+    def PLNtoDiffrent(self,kwota,KodWaluta):
+        KodWaluta = KodWaluta.upper()
+        try:
+            index = self.Code.index(KodWaluta)
+            KwotaWaluta = (kwota/float((self.Course[index]).replace(',','.')))*int(self.Converter[index])
+            print("{} PLN to {} {}".format(kwota,KwotaWaluta,KodWaluta))
+            return KwotaWaluta
+        except:
+            print("Kod waluty jest niepoprawny")
+    def DiffrentToDiffrent(self,kwota,KodWaluta1,KodWaluta2):
+        KodWaluta1 = KodWaluta1.upper()
+        KodWaluta2 = KodWaluta2.upper()
+        try:
+            index1 = self.Code.index(KodWaluta1)
+            index2 = self.Code.index(KodWaluta2)
+            KwotaWaluta = (kwota*float((self.Course[index1]).replace(',','.'))/float((self.Course[index2]).replace(',','.')))*(int(self.Converter[index2])/int(self.Converter[index1]))
+            print("{} {} to {} {}".format(kwota,KodWaluta1,KwotaWaluta,KodWaluta2))
+            return KwotaWaluta
+        except:
+            print("Kody walut są niepoprawne.")
+kursy = Kursy('kursy/kursy.xml')
+kursy.ListaKursow()
+kursy.PLNtoDiffrent(43,"usd")
+kursy.DiffrentToDiffrent(99,'huf','nok')
